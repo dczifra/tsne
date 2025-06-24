ab1b743433c20327062ad5e78fd8333bae8b07ac
Domonkos Czifra <doma945@gmail.com>
2025-06-24 15:32:28 +0200

    Docker run: update with gpus all

--- docker/run.sh
+++ docker/run.sh
@@ -1,5 +1,14 @@
+#!/bin/bash
+
+# CPU flag, if set, set GPU to '', else set to '--gpus all'
+if [ "$1" == "--cpu" ]; then
+    GPU_FLAG=""
+else
+    GPU_FLAG="--gpus all"
+fi
+
 docker run \
-    --rm \
+    --rm $GPU_FLAG \
     --mount type=bind,source="$(pwd)",target=/workspace \
     -it doma945/nvidia_base bash
 
