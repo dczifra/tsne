83e043625cb2f1b22246b84306a31609c50c98d4
Domonkos Czifra <doma945@gmail.com>
2025-10-25 22:46:21 +0200

    Add custom tsne

--- install_tsne.sh
+++ install_tsne.sh
@@ -1,7 +1,7 @@
 # === Install faiss ===
 #rm -rf tsne-cuda/build
-mkdir tsne-cuda/build
-cd tsne-cuda/build
+mkdir src/external/tsne-cuda/build
+cd src/external/tsne-cuda/build
 cmake ..
 make -j16
 cd python
